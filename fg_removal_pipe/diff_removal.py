4cf0ce83b49419b0dce7355334507936ffbf0583
Jen Feron <81229417+jenferon@users.noreply.github.com>
2026-01-26 14:00:25 +0000

    Update diff_removal.py

--- fg_removal_pipe/diff_removal.py
+++ fg_removal_pipe/diff_removal.py
@@ -67,3 +67,54 @@ def GPR(data, frequency):
     resids_gpr = data - model_gpr
 
     return model_gpr, resids_gpr
+
+def bootstrap_fun(data, generator):
+    """
+    Boostrap function for use in ICASSO
+
+    Inputs:
+    data (numpy array):
+    """
+    return data[generator.choice(range(data.shape[0]), size=data.shape[0]), :]
+
+def unmixing_fun(ica):
+    return ica.components_
+
+def icasso(data, comp_num, distance, itter=100):
+    """
+    Code to utilise ICASSO from Himberg et al (2014) for numpy data cubes
+    
+    Inputs:
+    data (numpy array): data to be fitted, assumed in the shape (npix,npix,nfreq)
+    comp_number (int): initial number of components to fit to
+    distance (float): distance length with which to define similarly clustered components
+    itter (int): number of iterations of the ICASSO algorithm to perform, taken as 100 if no number is given
+    
+    Outputs
+    model (numpy array): model produced by ICASSO in the shape (npix,npix,nfreq)
+    residuals (numpy array): residuals produced by data - model in the shape (npix,npix,nfreq)
+    """
+    # initial random seed
+    random_state = 10 
+    shape = data.shape
+    data_reshape = np.transpose(data.reshape((shape[0]*shape[1],shape[2])))
+    ica_params = {'n_components': comp_num} 
+    icasso1 = Icasso(FastICA, ica_params=ica_params, iterations=itter, bootstrap=False,
+                    vary_init=True)
+
+
+    icasso1.fit(data=data_reshape, fit_params={}, random_state=random_state,
+               bootstrap_fun=bootstrap_fun, unmixing_fun=unmixing_fun)
+
+    icasso1.plot_dendrogram()
+    W_, scores = icasso1.get_centrotype_unmixing(distance=distance)
+    
+    scores = np.array([scores])
+    
+    S_ = np.dot(W_, data_reshape.T)#ICs
+    A = linalg.pinv(W_, check_finite=False)
+    temp = (np.matmul(A,S_))
+    model = temp.reshape(shape[0],shape[1],shape[2])
+    resids = data - model
+    
+    return resids, model
