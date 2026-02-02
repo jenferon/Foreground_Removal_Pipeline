7d4a9070ee474dd1f2db2e8ba794956b1ded8a4e
jenferon <ppxjf3@odhar.physics.nottingham.ac.uk>
2026-02-02 10:14:04 +0000

    monday morning update, get jupyter-notebook working to make power spec

--- src/fg_removal_pipe/power_spectrum.py
+++ src/fg_removal_pipe/power_spectrum.py
@@ -1,20 +1,49 @@
 import tools21cm as t2c
+import numpy as np
+from scipy import interpolate
+import matplotlib.colors as colors
+from astropy.cosmology import FlatLambdaCDM
+import astropy.units as u
 
-kbins=15
-mubins=15
+cosmo = FlatLambdaCDM(H0=71 * u.km / u.s / u.Mpc, Om0=0.27)
+plotting_scale={'x': 'log', 'y': 'log', 'z': 'log'}
 
+
+def zFromNu(nu):
+  """
+  Convert frequency of 21cm line to redshift
+    
+  Input: nu [MHz]
+  """
+  nu21 = 1.420405e3  #MHz
+  return nu21/nu - 1.0
   
-i0 = 0
-i1 = 101  
+
+def get_lengths(nu0, nu1, theta):
+  """
+  Docstring for get_lengths
+    
+  :param nu0: Description
+  :param nu1: Description
+  :param FoV: Description
+  """
+  nu_mid = (nu0+nu1)/2
+  z_lo = zFromNu(nu0)
+  z_mid = zFromNu(nu_mid)
+  z_hi = zFromNu(nu1)
+    
+  L_para = cosmo.comoving_distance(z_lo) - cosmo.comoving_distance(z_hi)
+  L_perp = cosmo.comoving_distance(z_mid) * (np.pi * theta / 180.0)
+  return L_para/u.Mpc, L_perp/u.Mpc
+
+
 def find_box_dims(frequency, FoV):
-  L_para, L_perp = get_lengths(frequency[i0], frequency[i1], (frequency[i0]+frequency[i1])/2, FoV)
+  L_para, L_perp = get_lengths(frequency[0], frequency[-1], FoV)
   box_dims = [L_perp, L_perp, L_para]
 
   return box_dims
 
-L_para, L_perp = get_lengths(freq[i0], freq[i1], (freq[i0]+freq[i1])/2, 1.0)
-box_dims = [L_perp, L_perp, L_para]
-def mu_powerspec(data,nkbins,nmubins,frequency):
+def mu_powerspec(data, nkbins, nmubins, frequency, FoV):
   """
   Calculating the power spectrum in mu and k
 
@@ -45,25 +74,44 @@ def mu_powerspec(data,nkbins,nmubins,frequency):
   return dk, kbins, mubins, err
 
 def powerspec_1d(data,kbins,frequency,FoV):
+  """
+  Docstring for powerspec_1d
+  
+  :param data: Description
+  :param kbins: Description
+  :param frequency: Description
+  :param FoV: Description
+  """
 
   box_dims = find_box_dims(frequency, FoV)
 
-  p_lc_eor_res, k_lc_eor_res, n_lc_res = t2c.power_spectrum_1d(resids_LC_FG[:,:,i0:i1], kbins=kbins, box_dims=box_dims, binning =  'log', return_n_modes=True)
-  d_lc = (p_lc_eor*k_lc_eor**3)/(2*np.pi**2)
-  err_rsd_res= np.empty_like(n_rsd_res)
+  p, k, n = t2c.power_spectrum_1d(data, kbins=kbins, box_dims=box_dims, binning =  'log', return_n_modes=True)
+  d = (p*k**3)/(2*np.pi**2)
+  err= np.empty_like(n)
 
-for ii in range(0,n_rsd_res.shape[0]):
-    err_lc_res[ii] = d_lc_res[ii]/np.sqrt(n_lc_res[ii])
-
-def cyclindircal_powerspec(data,kbins,frequency):
-
-   box_dims = find_box_dims(frequency, FoV)
+  for ii in range(0,n.shape[0]):
+    err[ii] = d[ii]/np.sqrt(n[ii])
   
-  pp, kper, kpar= t2c.power_spectrum_2d(data, kbins=bins, box_dims=box_dims, return_modes=False)
+  return d, k, err
+  
+
+def cyclindircal_powerspec(data, kbins, frequency, FoV):
+  """
+  Docstring for cyclindircal_powerspec
+  
+  :param data: Description
+  :param kbins: Description
+  :param frequency: Description
+  :param FoV: Description
+  """
+
+  box_dims = find_box_dims(frequency, FoV)
+  
+  pp, kper, kpar= t2c.power_spectrum_2d(data, kbins=kbins, box_dims=box_dims, return_modes=False)
 
   #normalise to dimensionless power spectrum
   for ii in range(0,len(kper)):
-      for jj in range(0,len(kpar_RSD)):
+      for jj in range(0,len(kpar)):
           pp[ii,jj] = (pp[ii,jj]*np.sqrt(kper[ii]**2+kpar[jj]**2)**3)/(2*np.pi**2)
   
   fp = interpolate.interp2d(kper, kpar, pp.T, kind='linear')
